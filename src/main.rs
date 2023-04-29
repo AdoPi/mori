dee8687ff6351035b36126d428c9eace0b1d92ec
Adonis Najimi <adopi.naj@gmail.com>
2023-04-29 16:01:28 +0200

    Funny new idea: show process usage

--- src/main.rs
+++ src/main.rs
@@ -26,14 +26,27 @@ fn ui_build_tree(ui: &mut egui::Ui) {
     pstree_userspace.unwrap().ui(ui);
 }
 
+
 impl eframe::App for MoriTreeApp {
 
    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
-       egui::CentralPanel::default()
-           .show(ctx, |ui| {
+
+       egui::SidePanel::left("Mori process").show(ctx, |ui| {
                ScrollArea::vertical().show(ui, |ui| {
                    ui_build_tree(ui);
                });
+       });
+
+       egui::CentralPanel::default().show(ctx, |ui| {
+           // TODO history of process
+               use egui::plot::{Line, Plot, PlotPoints};
+               let sin: PlotPoints = (0..1000).map(|i| {
+                   let x = i as f64 * 0.01;
+                   [x, x.sin()]
+               }).collect();
+               let line = Line::new(sin);
+               Plot::new("Usage").view_aspect(2.0).show(ui, |plot_ui| plot_ui.line(line));
+
        });
 
        // TODO: repaint every x ms, not every frame
